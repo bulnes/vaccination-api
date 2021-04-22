fa7cd457e01f1ff4d0f7b36298aeb8e34ee4977c
Bruno Bulnes <bbulnes@sp.r7.com>
2021-04-22 11:51:19 -0300

    fix: app env port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 const config = require('./config');
 
-const { app_host: host, app_port: port } = config;
+const { app_host: host, app_port } = config;
+const port = process.env.PORT || app_port
 
 app.listen(port, () => console.log(`App listening on ${host}:${port}`));
